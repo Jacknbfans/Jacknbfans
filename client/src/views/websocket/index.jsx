05a56fe4a4b3b8ce9502b232692bbe2c80c26af0
Jacknbfans <vc3island@hotmail.com>
2024-02-15 02:17:42 -0500

    202402151615 commit

--- client/src/views/websocket/index.jsx
+++ client/src/views/websocket/index.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import './ws.css'
 
+const ws = new WebSocket('ws://104.128.95.54:3030/websockets/test')
+ws.onopen = e => {
+  console.log('WebSocket 连接状态： ' + ws.readyState)
+}
 
+function handleClick() {
+  
+    const msgBox = document.getElementById('msg-need-send');
+    ws.send(msgBox.value);
+} 
+
+ws.onmessage = data => {
+  const receiveBox = document.getElementById('receive-box')
+  receiveBox.innerHTML += '<p>' + data.data + '</p>'
+  receiveBox.scrollTo({
+    top: receiveBox.scrollHeight,
+    behavior: "smooth"
+  })
+}
+
+  
 class Websocket extends Component {
 
     constructor(props) {
@@ -19,27 +40,32 @@ class Websocket extends Component {
         this.callAPI();
     }
 
+    handleClick() {
+      ws.close();
+      console.log('close');
+
+
+  } 
+
+
     render(){
         return (
-            <>
-            <meta charSet="UTF-8" />
-            <title>Title</title>
-            <link rel="stylesheet" href="./ws.css" />
-            <script src="../websocket.js" />
-            <div className="websocket">
-                <div className="receive">
-                <p>服务端返回的消息</p>
-                <div id="receive-box" />
-                </div>
-                <div className="send">
-                <textarea type="text" id="msg-need-send" defaultValue={""} />
-                <p>
-                    <button id="send-btn">点击发消息给服务端</button>
-                </p>
-                </div>
-                <button id="exit">关闭连接</button>
-            </div>
-            </>
+
+
+          <div className="websocket">
+          <div className="receive">
+            <p>服务端返回的消息</p>
+            <div id="receive-box" />
+          </div>
+          <div className="send">
+            <textarea type="text" id="msg-need-send" defaultValue={"666"} />
+            <p>
+              <button id="send-btn" onClick={handleClick}>点击发消息给服务端</button>
+            </p>
+          </div>
+          <button id="exit" onClick={this.handleClick}>关闭连接</button>
+        </div>
+        
 
         );
       }
