78cfbc6a3fa03d83d894815aa9b8d5a3dfd3cb00
Jacknbfans <vc3island@hotmail.com>
2024-02-17 22:46:34 -0500

    202402181245 commit

--- client/src/views/websocket/index.jsx
+++ client/src/views/websocket/index.jsx
@@ -2,26 +2,22 @@ import React, { Component, useEffect } from 'react';
 import '../../css/ws.css'
 import { withRouter, Prompt  } from 'react-router-dom'
 
-const ws = new WebSocket('ws://104.128.95.54:3030/websockets/test')
-/* ws.onopen = e => {
-  console.log('WebSocket 连接状态： ' + ws.readyState)
+const ws = new WebSocket('ws://104.128.95.54:3030/websockets/test');
+
+window.addEventListener('scroll', () => { 
+  var h = window.innerHeight;
+  var j = document.documentElement.scrollHeight;
+  var k = document.documentElement.scrollTop;
+  if ((h + k) >= j - 150) {
+      console.log('add scroll');
+  }
+});
+
+window.onbeforeunload = function(e){
+  ws.close();
+  console.log('beforeunload:'+e);
 }
 
-ws.onmessage = data => {
-  const receiveBox = document.getElementById('receive-box')
-  receiveBox.innerHTML += '<p>' + data.data + '</p>'
-  receiveBox.scrollTo({
-    top: receiveBox.scrollHeight,
-    behavior: "smooth"
-  })
-} */
-
-function handleClick() {
-  
-  const msgBox = document.getElementById('msg-need-send');
-  ws.send(msgBox.value);
-} 
-  
 class Websocket extends Component {
 
     constructor(props) {
@@ -41,16 +37,6 @@ class Websocket extends Component {
     componentDidMount() {
         this.callAPI();
         console.log('componentDidMount');
-        document.getElementById('exit').addEventListener('click',this.handleClick);
-
-/*         window.addEventListener('scroll', () => { 
-          var h = window.innerHeight;
-          var j = document.documentElement.scrollHeight;
-          var k = document.documentElement.scrollTop;
-          if ((h + k) >= j - 150) {
-              console.log('add scroll');
-          }
-        }); */
 
         ws.onopen = e => {
           console.log('WebSocket 连接状态： ' + ws.readyState)
@@ -65,48 +51,68 @@ class Websocket extends Component {
           })
         }
 
-
+        document.getElementById('send-btn').addEventListener('click',this.startClick);
+        document.getElementById('exit').addEventListener('click',this.handleClick);
     }
 
     componentWillMount () {
       // 拦截判断是否离开当前页面
-      window.addEventListener('beforeunload', this.beforeunload);
+      //window.addEventListener('beforeunload', this.beforeunload);
       console.log('componentWillMount');
     }
 
     componentWillUnmount () {
       // 销毁拦截判断是否离开当前页面
-      window.removeEventListener('beforeunload', this.beforeunload);
+      //window.removeEventListener('beforeunload', this.beforeunload);
       //ws.close();
       console.log('componentWillUnmount');
     }
 
-    beforeunload (e) {
+/*     beforeunload (e) {
       ws.close();
       console.log('beforeunload:'+e);
-    }
+    } */
 
     handleClick() {
       ws.close();
-      console.log('this.handleClick');
-  } 
+      console.log('handleClick');
+    } 
+
+    startClick() {
+
+      ws.onopen = e => {
+        console.log('WebSocket 连接状态： ' + ws.readyState)
+      }
+      ws.onmessage = data => {
+        const receiveBox = document.getElementById('receive-box')
+        receiveBox.innerHTML += '<p>' + data.data + '</p>'
+        receiveBox.scrollTo({
+          top: receiveBox.scrollHeight,
+          behavior: "smooth"
+        })
+      }
+
+      const msgBox = document.getElementById('msg-need-send');
+      ws.send(msgBox.value);
+      console.log('startClick:'+msgBox.value);
+    } 
 
     render(){
         return (
-
           <div className="websocket">
             <div className="receive">
               <p>服务端返回的消息</p>
               <div id="receive-box" />
             </div>
             <div className="send">
-              <textarea type="text" id="msg-need-send" defaultValue={"666"} />
+              <textarea type="text" id="msg-need-send" defaultValue={"777"} />
               <p>
-                <button id="send-btn" onClick={handleClick}>点击发消息给服务端</button> 
+                {/* <button id="send-btn" onClick={handleClick}>点击发消息给服务端</button>  */}
+                <button id="send-btn">点击发消息给服务端</button> 
               </p>
             </div>
             {/* <button id="exit" onClick={this.handleClick}>关闭连接</button> */}
-            <button id="exit" >关闭连接</button>
+            <button id="exit">关闭连接</button>
         </div>
         );
       }
