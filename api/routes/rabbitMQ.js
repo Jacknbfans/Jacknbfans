29d887e7690ddb6e0d4e2843ec270a33436d5e01
Jacknbfans <vc3island@hotmail.com>
2024-02-26 04:26:18 -0500

    202402261830 commit

--- api/routes/rabbitMQ.js
+++ api/routes/rabbitMQ.js
@@ -19,9 +19,62 @@ const rmq = require('../lib/RabbitMQ');
 };  */
 
 
+async function loginToRabbitMq() {
+  try {
+      // 创建与RabbitMQ服务器的连接
+      const connection = await amqp.connect({ url: 'amqp://localhost' });
+      
+      console.log("成功连接到 RabbitMQ");
+      
+      // 关闭连接
+      setTimeout(() => {
+          //connection.close();
+          //process.exit(0);
+          console.log('hehe');
+      }, 500);
+  } catch (error) {
+      console.error(`无法连接到 RabbitMQ: ${error}`);
+  }
+}
+
+loginToRabbitMq();
+
 router.get('/', (req, res) => {
 
-  amqp.connect('amqp://localhost')
+
+  try {
+    // 创建与RabbitMQ服务器的连接
+    const connection =  amqp.connect({ url: 'amqp://localhost' })
+    .then((connection) => connection.createChannel())
+    .then((channel) => {
+      // Channel is ready for use
+      channel.assertExchange('direct_exchange', 'direct', { durable: false });
+      channel.assertQueue('chat_messages', { durable: false });
+      channel.bindQueue('chat_messages', 'direct_exchange', 'chat');
+    })
+
+    //const message = req.body.message;
+
+    // Publish the message to RabbitMQ
+    //channel.publish('direct_exchange', 'chat', Buffer.from(message));
+  
+    //res.sendStatus(200);
+    
+    res.send("成功连接到 RabbitMQ");
+    
+    // 关闭连接
+    setTimeout(() => {
+        //connection.close();
+        //process.exit(0);
+        console.log('haha');
+    }, 500);
+} catch (error) {
+    //console.error(`无法连接到 RabbitMQ: ${error}`);
+    res.send(`无法连接到 RabbitMQ: ${error}`);
+}
+
+
+/*   amqp.connect('amqp://localhost')
     .then((connection) => connection.createChannel())
     .then((channel) => {
       // Channel is ready for use
@@ -33,8 +86,9 @@ router.get('/', (req, res) => {
       console.error('Error connecting to RabbitMQ', error);
     }); 
 
-    res.send('Real-Time Application');
+    res.send('Real-Time Application'); */
 });
+
 
 /* 
 router.post('/message', (req, res) => {
