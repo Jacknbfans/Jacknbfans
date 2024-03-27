d32c4314f893cf3444087a82050b01d868fc54b0
Jacknbfans <vc3island@hotmail.com>
2024-03-27 05:07:54 -0400

    202403271800 commit

--- app/kevin005/kevin005/ContentView.swift
+++ app/kevin005/kevin005/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let me = Resume.shared
     var body: some View {
-            Text("Hello, kevin!")
-                .padding()
+        VStack(spacing:30){
+            HStack{
+                Image("avatar")
+                    .resizable().aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width:200)
+                
+                VStack(spacing:20){
+                    Text(me.name)
+                        .font(.title).bold()
+                    Text(me.title)
+                    Label(me.location,systemImage: "eraser.fill").foregroundColor(.secondary)
+                    
+                }
+            }
+            Text(me.bio).font(.title3).lineSpacing(10)
+            
+            Text("Contact Me")
+                .foregroundColor(.white)
+                .font(.title2.weight(.medium))
+                .padding(.vertical,10)
+                .frame(maxWidth: .infinity)
+                .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.brown))
+            
+            
+            
+        }.padding()
     }
 }
 
